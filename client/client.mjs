c2dea2477d5466725d371207bb450bdd2c530140
Jake Deery <jake.deery@openfifth.co.uk>
2026-09-14 21:07:52 +0100

    s

--- client/client.mjs
+++ client/client.mjs
@@ -98,7 +98,7 @@ class Client {
     }
 
     serverMsg(netsocket, message) {
-        console.log("[DEBUG]\tnetsocket-message: " + message);
+        console.log("[DEBUG]\tnetsocket-message: " + message.data);
         /*if (message.data)
             return this._send_netsocket_message(netsocket, message.data);
         else return;*/
